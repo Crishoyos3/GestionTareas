c457c2a83e83366cede8f0987a5a6652a2a95669
Crishoyos3 <cristian.betancourth36965@ucaldas.edu.co>
2026-03-09 11:30:44 -0500

    Codigo completado 1

--- src/Clases/GestorTareas.java
+++ src/Clases/GestorTareas.java
@@ -1,4 +1,78 @@
 package Clases;
 
-public class gestorTareas {
+import java.util.ArrayList;
+
+public class GestorTareas {
+
+
+    private ArrayList<Tarea> listaTareas;
+
+    public GestorTareas() {
+        this.listaTareas = new ArrayList<>();
+    }
+    //agregamos una tarea a la ultima posicion de la lista O(1)
+    public void agregarTarea(Tarea tarea) {
+        listaTareas.add(tarea);
+    }
+
+    //recorremos toda la lista para mostrar todas las tareas 0(n)
+    public void listarTareas() {
+        if (listaTareas.isEmpty()) {
+            System.out.println("No hay tareas registradas.");
+            return;
+        }
+        for (Tarea t : listaTareas) {
+            System.out.println(t.toString());
+        }
+    }
+
+    //Recorremos la lista hasta encontrar la tarea que buscamos O(n)
+    public void buscarPorId(int id){
+        for (Tarea t : listaTareas) {
+            if(t.getId()== id){
+                System.out.println(t.toString());
+
+            }
+        }
+    }
+
+    //Recorremos la lista buscando la tarea que buscamos y que no esté completada O(n)
+    public void marcarCompletada(int id){
+        for (Tarea t : listaTareas) {
+            if(t.getId()== id && t.isCompletada() == false){
+                t.setCompletada(true);
+
+            }
+        }
+    }
+
+    //Eliminamos una tarea con el ID que buscamos O(1)
+    public void eliminarTarea(int id){
+
+
+                listaTareas.removeIf(tarea -> tarea.getId() == id );
+
+
+    }
+
+    //Se recorre toda la lista buscando tareas que no estén completadas O(n)
+    public void obtenerTareasPendientes(){
+        for (Tarea t : listaTareas) {
+            if(t.isCompletada() == false){
+                System.out.println(t.toString());
+
+            }
+        }
+    }
+
+    //Se recorre toda la lista buscando las tareas con la prioridad que buscamos O(n)
+    public void obtenerTareasPrioridad(int prioridad){
+        for (Tarea t : listaTareas) {
+            if(t.getPrioridad() == prioridad){
+                System.out.println(t.toString());
+
+            }
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,97 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import Clases.GestorTareas;
+import Clases.Tarea;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.Objects;
+import java.util.Scanner;
+
+void main() {
+
+
+    Scanner entrada = new Scanner(System.in);
+    //Instacia de la clase con los metodos
+    GestorTareas gestor = new GestorTareas();
+    //Variables
+    int opcion;
+    int id;
+    String nombreTarea,estado;
+    int prioridad;
+    boolean completada;
+
+
+    //Condicion para que no se cierre el sistema sin una orden
+    do {
+        System.out.println("1. Ingresar tarea");
+        System.out.println("2. Ver todas las tareas");
+        System.out.println("3. Ver tarea por id");
+        System.out.println("4. Ver tareas por prioridad");
+        System.out.println("5. ver tareas pendientes");
+        System.out.println("6. Marcar como completada");
+        System.out.println("7. Eliminar tarea");
+        System.out.println("0. Salir del sistema");
+        opcion = entrada.nextInt();
+
+        switch (opcion){
+            case 1:
+                System.out.println("Inserte id de la tarea: ");
+                id = entrada.nextInt();
+                System.out.println("Inserte nombre de la tarea: ");
+                entrada.nextLine();
+                nombreTarea = entrada.nextLine();
+                System.out.println("Inserte prioridad de la tarea (1-5):");
+                prioridad = entrada.nextInt();
+                System.out.println("Inserte estado de la tarea (Completada-Pendiente): ");
+                estado = entrada.next();
+
+                if (Objects.equals(estado, "Completada")){
+                    completada = true;
+                }else {
+                    completada = false;
+                }
+                gestor.agregarTarea(new Tarea(id,nombreTarea,prioridad,completada));
+                System.out.println("Se inserto correctamente la tarea");
+                break;
+
+            case 2:
+                gestor.listarTareas();
+                break;
+
+            case 3:
+                System.out.println("Ingrese el id de la tarea que desea buscar: ");
+                id = entrada.nextInt();
+                gestor.buscarPorId(id);
+                break;
+
+            case 4:
+                System.out.println("Inserte prioridad de las tareas que desea ver (1-5):");
+                prioridad = entrada.nextInt();
+                gestor.obtenerTareasPrioridad(prioridad);
+                break;
+
+
+            case 5:
+                gestor.obtenerTareasPendientes();
+                break;
+
+            case 6:
+                System.out.println("Ingrese el id de la tarea que desea marcar como completada: ");
+                id = entrada.nextInt();
+                gestor.marcarCompletada(id);
+                break;
+
+            case 7:
+                System.out.println("Ingrese el id de la tarea que desea eliminar: ");
+                id = entrada.nextInt();
+                gestor.eliminarTarea(id);
+                break;
+
+        }
+
+
+
+
+    }while(opcion != 0);
+
+
+
+
 }
